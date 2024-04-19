5e5e5ea96eade9a91ee6997a9e27f60825eceb39
Aamir Aziz <141019144+EnggAamir@users.noreply.github.com>
2024-04-19 12:52:11 +0500

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/c.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "laptop and man";
 
 /**
  * Project list
@@ -37,7 +37,7 @@ const projectList = [
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Data Science for Beginners",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
     url: "https://github.com/microsoft/web-dev-for-beginners",
